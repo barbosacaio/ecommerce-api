7f72f7aa489d1d1149692c57ab413c879cf97759
barboscaio <caiiohenrique2004@gmail.com>
2025-12-18 15:43:15 -0300

    Created base folder structure and split app/server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,6 @@
-import express from 'express';
+import { app } from './app';
 
-const app = express();
 const PORT = 3000;
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({message: 'eCommerce API is running'});
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
