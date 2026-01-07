c3ae92a457efa06d309076bb0f03589a8888512f
barboscaio <caiiohenrique2004@gmail.com>
2026-01-07 14:57:13 -0300

    Added password hashing using bcrypt

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import './config/loadEnv';
 import { app } from './app';
 import { env } from './config/env';
+import { comparePassword, hashPassword } from './services/password.service';
+import test from 'node:test';
 
 app.listen(env.port, () => {
   console.log(`Server is running on http://localhost:${env.port} in ${env.nodeEnv} mode`);
